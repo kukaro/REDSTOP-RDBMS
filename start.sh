aef6a87f0eb575715d0b4e5a031bfd9cb6c279d7
kukaro <justkukaro@naver.com>
2018-10-13 04:33:52 +0900

    com

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 sudo apt-get update
 sudo apt-get install mysql-server
 mysql_secure_installation
-
 # root 123redstop456
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+sudo systemctl start jenkins
