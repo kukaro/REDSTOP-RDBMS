45bab853f9ef3392cf4d7edd30920678107f110d
kukaro <justkukaro@naver.com>
2018-11-14 08:45:51 +0900

    4000->3000

--- docs/sql/create-init-data.sql
+++ docs/sql/create-init-data.sql
@@ -11,17 +11,17 @@ insert into rs_project(owner,project_name) values('demo', 'demo project');
 insert into rs_project(owner,project_name) values('kukaro', 'kukaro project');
 
 delete from rs_demo_urls;
-insert into rs_demo_urls(project_id,url,method,name) values('1', 'http://52.79.221.114:3000/api/json-test/single-json','get','SingleJson');
-insert into rs_demo_urls(project_id,url,method,name) values('1', 'http://52.79.221.114:3000/api/json-test/multi-json', 'get','MultiJson');
-insert into rs_demo_urls(project_id,url,method,name) values('1', 'http://52.79.221.114:3000/', 'get',"WebRoot");
+insert into rs_demo_urls(project_id,url,method,name) values('1', 'http://13.209.126.84:4000/api/json-test/single-json','get','SingleJson');
+insert into rs_demo_urls(project_id,url,method,name) values('1', 'http://13.209.126.84:4000/api/json-test/multi-json', 'get','MultiJson');
+insert into rs_demo_urls(project_id,url,method,name) values('1', 'http://13.209.126.84:4000/', 'get',"WebRoot");
 
 delete from rs_kukaro_urls;
-insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://52.79.221.114:3000/api/json-test/single-json', 'get','SingleJson');
-insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://52.79.221.114:3000/api/json-test/multi-json', 'get','MultiJson');
-insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://52.79.221.114:3000/', 'get','WebRoot');
-insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://52.79.221.114:3000/web/log-in', 'get','WebLogin');
-insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://52.79.221.114:3000/web/log-out', 'get','WebLogout');
-insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://52.79.221.114:3000/web/sign-up', 'get','WebSignUp');
+insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://13.209.126.84:4000/api/json-test/single-json', 'get','SingleJson');
+insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://13.209.126.84:4000/api/json-test/multi-json', 'get','MultiJson');
+insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://13.209.126.84:4000/', 'get','WebRoot');
+insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://13.209.126.84:4000/web/log-in', 'get','WebLogin');
+insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://13.209.126.84:4000/web/log-out', 'get','WebLogout');
+insert into rs_kukaro_urls(project_id,url,method,name) values('2', 'http://13.209.126.84:4000/web/sign-up', 'get','WebSignUp');
 
 delete from rs_2_scenario;
 insert into rs_2_scenario(type,iterate_period) values('A','00:00:10');
